2add86c5930ac304644479b9b464e322c02623e6
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-11 17:26:16 +0900

    :wrench: Add SecurityConfig
    ## Description
    - SecurityConfig 설정 파일 추가
    - 간단한 인가, 인증 정책 설정

---
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 인가 정책
+        http.authorizeRequests()
+            .anyRequest().authenticated();
+
+        // 인증 정책
+        http
+            .formLogin();
+
+        return http.build();
+    }
+}
+
