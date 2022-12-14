31655fc30493673270ed7e95ac3c7d17b1bdb286
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-14 19:11:55 +0900

    :sparkles: User Controller, Service, Repository 추가
    ## Description
    - 회원 관련 MVC 추가

---
+++ src/main/java/io/security/corespringsecurity/service/implement/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package io.security.corespringsecurity.service.implement;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import io.security.corespringsecurity.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Override
+    public String passwordEncoding(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    @Transactional
+    @Override
+    public void createUser(Account account) {
+        userRepository.save(account);
+    }
+}
