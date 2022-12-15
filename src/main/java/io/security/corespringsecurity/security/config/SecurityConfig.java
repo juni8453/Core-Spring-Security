dc328402ddec3afcb6a23088860b764eff7eac8b
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-15 19:50:01 +0900

    :wrench: SecurityConfig Bean 등록 객체 삭제
    ## Description
    - Bean 객체로 등록한 AuthenticationManager 객체 등록 삭제

--- src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package io.security.corespringsecurity.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -12,22 +11,16 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    // DB 와 연동한 인증처리를 위해 사용하는 AuthenticationManager 객체
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
-        return auth.getAuthenticationManager();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    // 보안 필터를 적용할 필요가 없는 정적 리소스 ignore 처리
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
